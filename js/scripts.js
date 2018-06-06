d30a3db2a6628ad93a78038afcd203cf09abf12d
Anousone Kaseumsouk, Jean Jia and Lan Dam <jean@>
2018-06-06 09:40:49 -0700

    Enter business logic code for second spec, it works.

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,20 @@
 // Business logic
 var translator = function(string){
+  var splitArrays = string.split("");
+  if(splitArrays.length === 1 && splitArrays[0].toUpperCase() ==="A" || splitArrays[0].toUpperCase() ==="E" || splitArrays[0].toUpperCase() ==="I" || splitArrays[0].toUpperCase() ==="O" || splitArrays[0].toUpperCase() ==="U") {
+    splitArrays.push("ay");
+    return joinString(splitArrays);
+  }
   return string;
 };
+
+var joinString = function(stringArray){
+  var string = "";
+  for (i = 0; i < stringArray.length; i += 1){
+    string = string.concat(stringArray[i]);
+  }
+  return string;
+}
 
 
 
