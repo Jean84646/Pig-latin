b9d27ae7b63ff14c9ef65f583de94cbdf2c10587
Anousone Kaseumsouk, Jean Jia and Lan Dam <anousone@>
2018-06-06 09:23:42 -0700

    Add code for first case, it works.

--- js/scripts.js
+++ js/scripts.js
@@ -1,9 +1,24 @@
+// Business logic
+var translator = function(string){
+  return string;
+};
+
+
+
+
+
+
+
+
+
+// User interface
 $(document).ready(function() {
   $("#input-form").submit(function(event) {
     event.preventDefault();
     var userInput = $("input#user-input").val();
 
-
+  var output = translator(userInput);
+  $("#user-output").text(output);
   });
 
 
